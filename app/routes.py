52ad8943b3278b5861ea1f367793b39bad9caf84
Dominik Rejman <dominik.rejman@nokia.com>
2024-01-29 21:03:31 +0000

    Part 3

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 
 from app.config import settings
+from app.crud import CRUD
 
 
 templates = Jinja2Templates(directory=settings.TEMPLATE_DIR)
@@ -9,17 +10,16 @@ router = APIRouter()
 
 
 @router.get("/")
-def index():
-    return """
-        <html>
-            <head>
-                <title>Simple Site</title>
-            </head>
-            <body>
-                <h1>Hello World!</h1>
-            </body>
-        </html>
-        """
+def index(request: Request):
+    db = CRUD.with_table("artist_info")
+    random_artist = db.get_random_item()
+    return templates.TemplateResponse(
+        "main.html",
+        {
+            "request": request,
+            "artist": random_artist,
+        }
+    )
 
 
 @router.get("/hello")
@@ -43,3 +43,31 @@ def main(request: Request):
             "page_title": "Main page",
         }
         )
+
+
+@router.get("/catalog")
+def catalog(request: Request):
+    db = CRUD.with_table("artist_details")
+    artists = db.all_items()
+    return templates.TemplateResponse(
+        "catalog.html",
+        {
+            "request": request,
+            "artists": artists,
+        }
+    )
+
+@router.get("/artist/{artist_id}")
+def artist(request: Request, artist_id: int):
+    db = CRUD.with_table("artist_info")
+    artist = db.find(key="id", value=artist_id)[0]
+    db = CRUD.with_table("artist_details")
+    artist_details = db.find(key="id", value=artist_id)[0]
+    return templates.TemplateResponse(
+        "artist.html",
+        {
+            "request": request,
+            "artist": artist,
+            "details": artist_details,
+        }
+    )
